154aa204db2667fbaf9225c781d27d23f3179721
cesar perez <inbioslab@gmail.com>
2025-05-07 01:34:29 -0500

    Updates

---
+++ js/clases.js
@@ -0,0 +1,11 @@
+export class Product {
+    constructor(id, producto, area, lote, stock, ingreso, vence){
+        this.id = id;
+        this.producto = producto;
+        this.area = area;
+        this.lote = lote;
+        this.stock = stock;
+        this.ingreso = ingreso;
+        this.vence = vence;
+    }
+}
