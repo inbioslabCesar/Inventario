a4639c0d7ccef82b4df7bd52f7db42dd9e081deb
cesar perez <inbioslab@gmail.com>
2025-05-07 23:59:15 -0500

    Se agrego la configuracion CRUD

--- src/js/almacen.js
+++ src/js/almacen.js
@@ -4,6 +4,39 @@ import { Product } from "./clases.js";
 const info = await listProducts();
 
 const cuerpoTabla = document.querySelector("#cuerpo-tabla");
+const myModal = new bootstrap.Modal(document.getElementById("modalProduct"));
+
+let idProductoUpdate = null;
+
+window.mostrarModal = (id) => {
+  idProductoUpdate = id;
+
+  let index = info.findIndex((item) => item.id == idProductoUpdate);
+
+  document.querySelector("#productoModal").value = info[index].name;
+  document.querySelector("#areaModal").value = info[index].area;
+  document.querySelector("#loteModal").value = info[index].lote;
+  document.querySelector("#stockModal").value = info[index].stock;
+  document.querySelector("#ingresoModal").value = info[index].ingreso;
+  document.querySelector("#venceModal").value = info[index].vence;
+
+  myModal.show();
+};
+
+const productUpdate = (event) => {
+  event.preventDefault();
+  let index = info.findIndex((item) => item.id == idProductoUpdate);
+
+  info[index].name = document.querySelector("#productoModal").value;
+  info[index].area = document.querySelector("#areaModal").value;
+  info[index].lote = document.querySelector("#loteModal").value;
+  info[index].stock = document.querySelector("#stockModal").value;
+  info[index].ingreso = document.querySelector("#ingresoModal").value;
+  info[index].vence = document.querySelector("#venceModal").value;
+
+  cargarTabla();
+  myModal.hide();
+};
 
 //variables para la paginacion
 
@@ -14,14 +47,14 @@ let paginaActiva = 1;
 
 let arreglo = info.slice(desde, limite);
 
-const cargarProductos = () => {
+const cargarTabla = () => {
   cuerpoTabla.innerHTML = "";
 
-  arreglo.map((producto) => {
+  info.map((producto) => {
     const fila = document.createElement("tr");
     fila.setAttribute("key", producto.id);
 
-    const contenido = `
+    const celdas = `
               <th scope="row">${producto.id}</th>
               <td>${producto.name}</td>
               <td>${producto.area}</td>
@@ -30,13 +63,13 @@ const cargarProductos = () => {
               <td>${producto.ingreso}</td>
               <td>${producto.vence}</td>
               <td>
-                  <button class="btn btn-sm btn-primary"><i class="fa-solid fa-pencil"></i></button>
-                  <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+                  <button class="btn btn-sm btn-primary" onclick="mostrarModal(${producto.id})"><i class="fa-solid fa-pencil"></i></button>
+                  <button class="btn btn-sm btn-danger" onclick="borrarProduct(${producto.id})"><i class="fa-solid fa-trash-can"></i></button>
               </td>
 
     `;
 
-    fila.innerHTML = contenido;
+    fila.innerHTML = celdas;
     cuerpoTabla.append(fila);
   });
   cargarItemPaginacion();
@@ -59,7 +92,7 @@ const cargarItemPaginacion = () => {
 
 const modificarArregloProductos = () => {
   arreglo = info.slice(desde, limite * paginaActiva);
-  cargarProductos();
+  cargarTabla();
 };
 
 window.pasarPagina = (pagina) => {
@@ -90,22 +123,39 @@ window.previusPage = () => {
 // Agregar un producto
 
 const agregarProduct = (event) => {
-    event.preventDefault()
+  event.preventDefault();
   let id = info.at(-1).id + 1;
-  let producto = document.querySelector("#producto").value;
+  let name = document.querySelector("#producto").value;
   let area = document.querySelector("#area").value;
   let lote = document.querySelector("#lote").value;
   let stock = document.querySelector("#stock").value;
   let ingreso = document.querySelector("#ingreso").value;
   let vence = document.querySelector("#vence").value;
-  
-  info.push(new Product(id, producto, area, lote, stock, ingreso, vence));
-  console.log(info)
+
+  info.push(new Product(id, name, area, lote, stock, ingreso, vence));
   document.querySelector("#formProduct").reset();
-  cargarProductos();
+  cargarTabla();
 };
 
+window.borrarProduct = (id) => {
+  let index = info.findIndex((item) => item.id == id);
 
-document.querySelector("#formProduct").addEventListener("submit", () => agregarProduct);
+  let validar = confirm(
+    `Está seguro/a que quiere eliminar el producto ${info[index].name}?`
+  );
 
-cargarProductos();
+  if (validar) {
+    info.splice(index, 1);
+    cargarTabla();
+  }
+};
+
+cargarTabla();
+
+document
+  .querySelector("#formProduct")
+  .addEventListener("submit", agregarProduct);
+
+document
+  .querySelector("#modalProduct")
+  .addEventListener("submit", productUpdate);
